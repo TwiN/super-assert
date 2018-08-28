c53fc5839bdff0bf35c75c44e24d6ac4ec76bb48
TwinProduction <christian-ct@hotmail.com>
2018-08-28 18:52:02 -0400

    Reduced duplicated code by using method overloading

--- src/main/java/org/twinnation/superassert/SuperAssert.java
+++ src/main/java/org/twinnation/superassert/SuperAssert.java
@@ -6,14 +6,6 @@ import java.util.List;
 
 public class SuperAssert {
 	
-	public static boolean notNull(Object object, String message) {
-		if (object == null) {
-			throw new IllegalArgumentException(message);
-		}
-		return true;
-	}
-	
-	
 	public static <T extends Exception> boolean notNull(Object object, T customException) throws T {
 		if (object == null) {
 			throw customException;
@@ -22,11 +14,8 @@ public class SuperAssert {
 	}
 	
 	
-	public static boolean isTrue(boolean b, String message) {
-		if (!b) {
-			throw new IllegalArgumentException(message);
-		}
-		return true;
+	public static boolean notNull(Object object, String message) {
+		return notNull(object, new IllegalArgumentException(message));
 	}
 	
 	
@@ -38,21 +27,18 @@ public class SuperAssert {
 	}
 	
 	
-	public static boolean isFalse(boolean b, String message) {
-		return isTrue(!b, message);
+	public static boolean isTrue(boolean b, String message) {
+		return isTrue(b, new IllegalArgumentException(message));
 	}
 	
 	
 	public static <T extends Exception> boolean isFalse(boolean b, T customException) throws T {
 		return isTrue(!b, customException);
 	}
+
 	
-	
-	public static boolean notEmpty(List<?> list, String message) {
-		if (list.isEmpty()) {
-			throw new IllegalArgumentException(message);
-		}
-		return true;
+	public static boolean isFalse(boolean b, String message) {
+		return isTrue(!b, message);
 	}
 	
 	
@@ -64,13 +50,18 @@ public class SuperAssert {
 	}
 	
 	
-	public static boolean notEmptyOrNull(List<?> list, String message) {
-		return notNull(list, message) && notEmpty(list, message);
+	public static boolean notEmpty(List<?> list, String message) {
+		return notEmpty(list, new IllegalArgumentException(message));
 	}
 	
 	
 	public static <T extends Exception> boolean notEmptyOrNull(List<?> list, T customException) throws T {
 		return notNull(list, customException) && notEmpty(list, customException);
+	}
+	
+	
+	public static boolean notEmptyOrNull(List<?> list, String message) {
+		return notNull(list, message) && notEmpty(list, message);
 	}
 
 }
